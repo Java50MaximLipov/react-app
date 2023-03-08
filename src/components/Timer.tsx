73c2a03ed6e8bc4c3dac9dd96cd10a9d61283a5c
mlipov <maxim.lipov@gmail.com>
2023-03-08 22:00:59 +0200

    HW-32 Timer-App

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,29 +1,30 @@
 import React, { useState, useEffect } from "react";
 import timeZones from "../time-zones";
+
 type Props = {
     cityCountry: string;
 }
 export const Timer: React.FC<Props> = ({ cityCountry }) => {
     const styles: React.CSSProperties = {
-        backgroundColor: "lightblue",
+        // backgroundColor: "lightblue",
         fontSize: "2em"
     };
-
     const [time, setTime] = useState<Date>(new Date());
-
     function tic() {
         setTime(new Date());
-
     }
-
     useEffect(() => {
-        const interval = setInterval(tic, 2000);
-        console.log("useEffect");
+        const interval = setInterval(tic, 1000);
         return () => clearInterval(interval);
-    }, [])
+    }, []);
 
-    return <div>
+    const timeZoneIndex = timeZones.findIndex(
+        (index) => (JSON.stringify(index)).includes(cityCountry)
+    );
+    const finalTimeZone = timeZoneIndex !== -1 ? timeZones[timeZoneIndex].name : undefined;
+
+    return <div className="box">
         <h2 >Current Time in {cityCountry}</h2>
-        <p style={styles}>{time.toLocaleTimeString(undefined, { timeZone: timeZones[3].name })}</p>
+        <p style={styles}>{time.toLocaleTimeString("en-GB", { timeZone: finalTimeZone})}</p>
     </div>
 }
