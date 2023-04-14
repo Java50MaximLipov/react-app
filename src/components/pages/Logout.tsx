331efdb89c207c2adb7875caf28c9b75c3d80942
mlipov <maxim.lipov@gmail.com>
2023-04-14 09:11:38 +0300

    HW-40 React Application Solution

---
+++ src/components/pages/Logout.tsx
@@ -0,0 +1,13 @@
+import { Box, Button } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { authActions } from "../../redux/authSlice";
+export const Logout: React.FC = () => {
+  const dispatch = useDispatch();
+  return (
+    <Box>
+      <Button onClick={() => dispatch(authActions.logout())}>
+        Confirm Logout
+      </Button>
+    </Box>
+  );
+};
---
+++ src/components/pages/ShoppingCart.tsx
@@ -0,0 +1,3 @@
+export const ShoppingCart: React.FC = () => {
+  return <h2>ShoppingCart loadded</h2>;
+};
